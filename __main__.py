4bf3843b3ab05ade3f0611ff631ca799ce844ff2
David Prichard <davidprichard64@gmail.com>
2016-03-14 15:51:53 -0400

    Minimal Project
    Removed entertainment center, added __main__ module; running __main__
    produces a minimal, but correct, HTML webpage.

---
+++ __main__.py
@@ -0,0 +1,7 @@
+
+import media
+import fresh_tomatoes
+
+movies = media.movies
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -7,3 +7,26 @@ class Movie:
         self.trailer_youtube_id = trailer_youtube_id
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
+
+
+# movie = Movie('title', 'trailer', 'trailer_url', 'poster_url')
+
+movie1 = Movie('The Matrix',
+               'TRAILER-ID1',
+               'https://www.youtube.com/watch?v=m8e-FF8MsqU',
+               'https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg'
+               )
+
+movie2 = Movie('Lost in Translation',
+               'TRAILER-ID2',
+               'https://www.youtube.com/watch?v=sU0oZsqeG_s',
+               'https://upload.wikimedia.org/wikipedia/en/4/4c/Lost_in_Translation_poster.jpg'
+               )
+
+movie3 = Movie('The Life Aquatic with Steve Zissou',
+               'TRAILER-ID3',
+               'https://www.youtube.com/watch?v=yh401Rmkq0o',
+               'https://upload.wikimedia.org/wikipedia/en/7/7c/Lifeaquaticposter.jpg'
+               )
+
+movies = [movie1, movie2, movie3]
