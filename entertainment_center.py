549a69b1ea093f9b783849fd42e54a2ec5151c8c
David Prichard <davidprichard64@gmail.com>
2016-03-14 12:29:11 -0400

    First commit

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+romCom = media.Movie("a", "b", "c", "d")
+action = media.Movie("g", "h", "d", "f")
+comedy = media.Movie("r", "e", "w", "p")
+
+movies = [romCom, action, comedy]
---
+++ media.py
@@ -0,0 +1,9 @@
+
+
+class Movie:
+
+    def __init__(self, title, trailer_youtube_id, trailer_youtube_url, poster_image_url):
+        self.title = title
+        self.trailer_youtube_id = trailer_youtube_id
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
