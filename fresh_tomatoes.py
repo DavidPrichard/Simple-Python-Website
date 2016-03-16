3ab3f03ea0e6667676d10b49e3835f6bbada031e
David Prichard <davidprichard64@gmail.com>
2016-03-16 13:54:29 -0400

    Added more movies

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -177,5 +177,32 @@ if __name__ == '__main__':
                    '1996'
                    )
 
-    movies = [movie1, movie2, movie3, movie4]
+    movie5 = Movie('Star Wars',
+                   'A long time ago,<br>in a galaxy far, far away.',
+                   'https://www.youtube.com/watch?v=1g3_CFmnU7k',
+                   'https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg',
+                   '#685b75',
+                   'George Lucas',
+                   '1977'
+                   )
+
+    movie6 = Movie('Citizen Kane',
+                   'It\'s terrific!',
+                   'https://www.youtube.com/watch?v=zyv19bg0scg',
+                   'https://upload.wikimedia.org/wikipedia/en/c/ce/Citizenkane.jpg',
+                   '#f9f5a2',
+                   'Orson Welles',
+                   '1941'
+                   )
+
+    movie7 = Movie('It\'s a Wonderful Life',
+                   'Every time a bell rings, an angel gets its wings.',
+                   'https://www.youtube.com/watch?v=LJfZaT8ncYk',
+                   'https://upload.wikimedia.org/wikipedia/en/9/95/Its_A_Wonderful_Life_Movie_Poster.jpg',
+                   '#FFC678',
+                   'Frank Capra',
+                   '1946'
+                   )
+
+    movies = [movie1, movie2, movie3, movie4, movie5, movie6, movie7]
     open_movies_page(movies)
